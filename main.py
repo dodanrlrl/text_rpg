69cc01cfc22561e07af307198016fc2466492303
dodanrlrl <dodanrlrl@github.com>
2026-08-19 14:47:10 +0900

    feat: 영문 로딩 안내문 추가

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def print_title():
     print("=========================")
     print("  Epic Journey: Text RPG  ")
     print("=========================")
+
+if __name__ == "__main__": 
+    print_title()
+    print("Loading game... Please wait.") # 이 줄 추가
