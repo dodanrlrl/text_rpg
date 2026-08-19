c264bfb66f9835e8a8ba65dc8bc90c0aa7ac6c85
dodanrlrl <dodanrlrl@github.com>
2026-08-19 15:57:53 +0900

    fix: 텍스트 구조 수정

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def print_title():
     print("=========================")
-    print(" 전설의 시작: 텍스트 RPG(Epic Journey: Text RPG) ")
+    print(" 전설의 시작: 텍스트 RPG")
+    print(" epic journey: text rpg ")
     print("=========================")
     
 if __name__ == "__main__":
     print_title()
-    print("게임 로딩 중... 잠시만 기다려주세요.(loading... please wait...)") # 이 줄 추가
+    print("게임 로딩 중... 잠시만 기다려주세요.") # 이 줄 추가
+    print("loading... please wait...") # 이 줄 추가
