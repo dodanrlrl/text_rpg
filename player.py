192608dda835ac9fb038c9907f889a1f5e3b8edd
dodanrlrl <dodanrlrl@github.com>
2026-08-19 14:33:38 +0900

    feat: 플레이어 공격 기능 추가

--- player.py
+++ player.py
@@ -2,3 +2,7 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.hp = 100
+
+    def take_damage(self, amount):
+        self.hp -= amount
+        print(f"체력이 {self.hp} 남았습니다.")
