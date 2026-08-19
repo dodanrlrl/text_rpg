3fb5e2d67b6702d170944f114a5982c153f688b9
dodanrlrl <dodanrlrl@github.com>
2026-08-19 14:34:20 +0900

    feat: 플레이어 피격 기능 추가

--- player.py
+++ player.py
@@ -6,3 +6,7 @@ class Player:
     def take_damage(self, amount):
         self.hp -= amount
         print(f"체력이 {self.hp} 남았습니다.")
+
+    def take_damage(self, amount):
+        self.hp -= amount
+        print(f"체력이 {self.hp} 남았습니다.")
